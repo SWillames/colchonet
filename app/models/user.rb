2a0c381eb72415ead705f937d2cd1e951f5276a0
SWillames <sergiowoc@gmail.com>
2015-07-28 16:41:15 -0300

    Validações do modelo user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
-	
+	EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\Z/
+
+	validates_presence_of :email, :full_name, :location
+	validates_length_of :bio, minimum: 20, allow_blank: false
+	validates_format_of :email, with: EMAIL_REGEXP
+
+
+	has_secure_password
 end
