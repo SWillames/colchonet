f24f5727c7810c580607de83d4f5e196de4c19bf
SWillames <sergiowoc@gmail.com>
2015-07-29 23:10:31 -0300

    Corrigido erros na apresentação da aplicação.

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,8 @@
 class Room < ActiveRecord::Base
 
+	validates_presence_of :title, :description, :location
+	validates_length_of :description, minimum: 20, allow_blank: false
+	
 	def complete_name
 		"#{title}, #{location}"
 	end
