24ffec3eed367a420e7d414a71a6975815d6aaa2
SWillames <sergiowoc@gmail.com>
2015-08-06 23:29:18 -0300

    Configurando o colchonet com dois idiomas inglês e português, sendo esse segundo o idioma padrão.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  LOCALES = /en|pt\-BR/
+
+  scope ":locale", locale: LOCALES do
    resources :rooms	
    resources :users
+  end
 
+   get ':locale' => 'home#index', locale: LOCALES
    root 'home#index'
 end
