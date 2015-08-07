64d25d09462ffef8c099f5098b80b77d2a5e05e9
SWillames <sergiowoc@gmail.com>
2015-08-07 03:01:38 -0300

    Criando a funcionalidade de envio de email para confirmação de identidae.

---
+++ app/mailers/signup.rb
@@ -0,0 +1,15 @@
+class SignupMailer < ActionMailer::Base 	
+	default from: 'no-reply@colcho.net'
+
+	def confirm_email(user)
+		@user = user
+		@confirmation_link	= root_url
+	
+
+		mail({
+			to: user.mail,
+			bcc: [sign ups <signups@colcho.net],
+			subject: I18n.t('signup.confirm_email.subject')
+		})
+    end   
+end
